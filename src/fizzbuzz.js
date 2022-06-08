8c9c2c3833a6d8ee624c9c98ea4d0f3353108b15
Lewis <ljoncb@gmail.com>
2022-06-08 16:08:58 +0100

    added some line space

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,10 @@
 function fizzbuzz(lower, upper) {
   const result = []
+
   for (let i = lower; i <= upper; i++) {
     const isMultOf3 = i % 3 === 0
     const isMultOf5 = i % 5 === 0
+
     if (isMultOf3 && isMultOf5) {
       result.push('FizzBuzz')
     } else if (isMultOf3) {
