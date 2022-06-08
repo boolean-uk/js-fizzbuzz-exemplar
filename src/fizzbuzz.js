5a29426bcfa9f62e086e80c3eafcf33bb532137f
Lewis <ljoncb@gmail.com>
2022-06-08 16:01:04 +0100

    an example of fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,19 @@
-// TODO: Write your function in this file
+function fizzbuzz(lower, upper) {
+  const result = []
+  for (let i = lower; i <= upper; i++) {
+    const isMultOf3 = i % 3 === 0
+    const isMultOf5 = i % 5 === 0
+    if (isMultOf3 && isMultOf5) {
+      result.push('FizzBuzz')
+    } else if (isMultOf3) {
+      result.push('Fizz')
+    } else if (isMultOf5) {
+      result.push('Buzz')
+    } else {
+      result.push(i)
+    }
+  }
+  return result
+}
 
-// TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzbuzz
